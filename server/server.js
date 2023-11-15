1b32aec7bc184b28e9e8b04326e56c8be8422005
Antriko <creeperdrag772@gmail.com>
2023-11-15 23:47:01 +0000

    some stuff

--- server/server.js
+++ server/server.js
@@ -6,66 +6,122 @@ var activeUsers = []
 // facing
 // customisation
 
+const timeToString = (time) => {
+    return `${("0" + time.getHours()).slice(-2)}:${("0" + time.getMinutes()).slice(-2)}:${("0" + time.getSeconds()).slice(-2)}`
+}
+
 const server = Bun.serve({
     fetch(req, server) {
-        var name = new URL(req.url).searchParams.get("name")
-
-        // Authentication for duplicate users
-        if(activeUsers.includes(name)) throw new Response("Duplicate username", {status: 401})
-
-        activeUsers.push(name)
         server.upgrade(req, {
             data: {
-                joined: Date.now(),
-                name: name
+                joined: Date.now()
             }
         });
     },
     websocket: {
         open(ws) {
-            ws.subscribe("room")
-            console.log(`${ws.data.name} connected`)
-            console.log(JSON.stringify({type: "a", data: "a"}))
-            ws.send(JSON.stringify({type: "a", data: "a"}))
-            ws.publish("room", `${ws.data.name} connected`);
+            
+            console.log(`New connection attempt`)
+            
         },
         message(ws, message) {
+            // console.log(message)
             try {
                 var data = JSON.parse(message)
             } catch (_) {
-                throw new Response("Invalid JSON", {status: 401})
+                var data = {}
             }
 
+            if(data.type !== 'facing' && data.type !== 'coords') console.log(data.type)
             switch (data.type) {
+                case "auth":
+                    if(ws.data.name) return;
+
+                    console.log(data.data, data.data.username)
+                    if(activeUsers.includes(data.data.username)) {
+                        ws.send(JSON.stringify({
+                            "type": "authResponse",
+                            "success": false
+                        }))
+                        break
+                    }
+
+                    
+                    ws.subscribe("room")
+
+                    
+                    ws.send(JSON.stringify({
+                        "type": "authResponse",
+                        "success": true
+                    }))
+                    activeUsers.forEach(user => {
+                        ws.send(JSON.stringify({
+                            type: "initPlayer",
+                            data: {
+                                username: user,
+                                position: data.data.position
+                            }
+                        }))
+                    })
+
+
+                    ws.data.name = data.data.username
+                    activeUsers.push(ws.data.name)
+
+                    ws.publish("room", JSON.stringify({
+                        type: "initPlayer",
+                        data: {
+                            username: ws.data.name,
+                            position: data.data.position,
+                        }
+                    }))
+                    console.log(`${ws.data.name} authenticated`)
+                    break;
+
                 case "message":
-                    var time = new Date()
-                    var timeMessage = `${("0" + time.getHours()).slice(-2)}:${("0" + time.getMinutes()).slice(-2)}:${("0" + time.getSeconds()).slice(-2)}`
+                    // console.log('msg', data.data, ws.data.name)
                     ws.publish("room", JSON.stringify({
                         type: "message",
                         data: {
-                            time: timeMessage,
-                            name: ws.data.name,
+                            time: timeToString(new Date()),
+                            username: ws.data.name,
                             message: data.data
                         }
                     }))
                     break;
 
                 // TODO
-                case "movement":
+                case "coords":
+                    ws.publish("room", JSON.stringify({
+                        type: "coords",
+                        data: {
+                            user: ws.data.name,
+                            data: data
+                        }
+                    }))
+                    break;
+                case "facing":
+                    ws.publish("room", JSON.stringify({
+                        type: "facing",
+                        data: {
+                            user: ws.data.name,
+                            data: data
+                        }
+                    }))
                     break;
                 case "emote":
                     break;
 
                 default:
-                    console.log(`Unknown request ${data}`)
+                    console.log(`Unknown request ${JSON.stringify(data)}`)
                     break
             }
         },
         close(ws) {
             activeUsers = activeUsers.filter(e => e !== ws.data.name)
-            console.log(`${ws.data.name} disconnected`, activeUsers);
             ws.unsubscribe("room")
             ws.publish("room", `${ws.data.name} disconnected`);
+            console.log(`${ws.data.name} disconnected`, activeUsers);
         }
     },
 });
